bdc57354d590f6abba353f830c384e50f01a495d
Jonathan Lyons <jrl08d@my.fsu.edu>
2014-11-14 14:55:04 -0500

    added model specs for customer and release, addresses #5

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Customer, :type => :model do
+
+  context 'indexes' do
+    pending { should have_index(:name) }
+  end
+
+  context 'associations' do
+    it { should have_many(:deployments) }
+    it { should have_many(:releases, through: :deployments) }
+  end
+
+  #TODO How do we test these in Rails 4?
+  # context 'mass assignment' do
+  #   it { should allow_mass_assignment_of(:name) }
+  # end
+
+  context 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+  end
+
+
+end
--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Release, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'indexes' do
+    pending { should have_index(:version) }
+  end
+
+  context 'associations' do
+   it { should have_many(:deployments) }
+    it { should have_many(:customers, through: :deployments) }
+  end
+
+  #TODO How do we test these in Rails 4?
+  # context 'mass assignment' do
+  #   it { should allow_mass_assignment_of(:release_id) }
+  #   it { should allow_mass_assignment_of(:customer_id) }
+  # end
+
+  context 'validations' do
+    it { should validate_presence_of(:version) }
+    it { should validate_uniqueness_of(:version) }
+  end
+
+
 end
