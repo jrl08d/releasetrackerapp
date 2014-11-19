cf52abf498bb889b8b5c52c8041999d8be32ebd2
Jonathan Lyons <jrl08d@my.fsu.edu>
2014-11-19 09:29:55 -0500

    stashing changes

---
+++ app/assets/javascripts/customers.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+	
+
+    // Setup - add a text input to each footer cell
+    $('#customers_table tfoot th').each( function () {
+        var title = $('#example thead th').eq( $(this).index() ).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    } );
+ 
+    // DataTable
+    var table = $('#customers_table').DataTable();
+ 
+    // Apply the search
+    table.columns().eq( 0 ).each( function ( colIdx ) {
+        $( 'input', table.column( colIdx ).footer() ).on( 'keyup change', function () {
+            table
+                .column( colIdx )
+                .search( this.value )
+                .draw();
+        } );
+    } );
+} );
+
