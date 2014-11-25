e21e6361a168516279ac3717f23da3f1a5c43fb0
Jonathan Lyons <jrl08d@my.fsu.edu>
2014-11-25 13:20:42 -0500

    added live filtering for table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,25 @@
 //= require autocomplete-rails
 //= require bootstrap-sprockets
 //= require jquery.livesearch
+//= require jquery.dataTables.min
+//= require jquery.quicksearch
+//= require jquery.filtertable.min
 //= require turbolinks
 //= require_tree .
 
+$(function() {
+  initPage();
+});
+$(window).bind('page:change', function() {
+  initPage();
+});
+function initPage() {
+  // Page ready code...
+
+    
+
+
+
+$('input#q_name_cont').quicksearch('table tbody tr');                              
+
+}
