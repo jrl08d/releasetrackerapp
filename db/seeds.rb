230dfe345390daee9c032a16ff1edf9ffb76f18a
Douglas Hathaway <dmh.dade@gmail.com>
2014-11-26 11:02:27 -0500

    add missing "end"

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,32 +2,34 @@ unless Rails.env.production?
 
 
 
-if Rails.env != 'production'
+  if Rails.env != 'production'
 
-  Release.destroy_all
-  Deployment.destroy_all
-  Customer.destroy_all
-  User.destroy_all
+    Release.destroy_all
+    Deployment.destroy_all
+    Customer.destroy_all
+    User.destroy_all
 
-  customer_count = 20
-  release_count = 30
+    customer_count = 20
+    release_count = 30
 
-  # Populate Customers
-  customer_count.times do
-    FactoryGirl.create :customer
+    # Populate Customers
+    customer_count.times do
+      FactoryGirl.create :customer
+    end
+
+    # Populate Releases
+    release_count.times do |i|
+      FactoryGirl.create :release, version: "#{i + 1}.0.0"
+    end
+
+    # Populate Deployments
+    (customer_count * 3).times do
+      FactoryGirl.create :deployment_for_existing_data
+    end
+
+    # Seed Admin
+    FactoryGirl.create :admin, username: 'admin', password: 'admin'
+
   end
-
-  # Populate Releases
-  release_count.times do |i|
-    FactoryGirl.create :release, version: "#{i + 1}.0.0"
-  end
-
-  # Populate Deployments
-  (customer_count * 3).times do
-    FactoryGirl.create :deployment_for_existing_data
-  end
-
-  # Seed Admin
-  FactoryGirl.create :admin, username: 'admin', password: 'admin'
 
 end
